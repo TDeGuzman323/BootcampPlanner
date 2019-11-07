755682066e72a2392beaf75bd466b25e430a83b3
Tara DeGuzman <tara.deguzman323@gmail.com>
2019-11-06 18:50:09 -0800

    sticky header

---
+++ main.js
@@ -0,0 +1,9 @@
+var now = moment();
+
+$('time').each(function(i, e) {
+    var time = moment($(e).attr('datetime'));
+
+    if(now.diff(time, 'days') <= 1) {
+        $(e).html('<span>' + time.from(now) + '</span>');
+    }
+});
